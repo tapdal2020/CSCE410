454e438fd52866c9615fe5bce7f535e11ab2dda9
Taylor Williamson <txag2020>
2020-02-27 19:52:56 -0600

    adding basic functionality

--- P3-provided/page_table.C
+++ P3-provided/page_table.C
@@ -12,30 +12,48 @@ unsigned long PageTable::shared_size = 0;
 
 
 
-void PageTable::init_paging(ContFramePool * _kernel_mem_pool,
-                            ContFramePool * _process_mem_pool,
-                            const unsigned long _shared_size)
-{
-   assert(false);
+void PageTable::init_paging(ContFramePool * _kernel_mem_pool,ContFramePool * _process_mem_pool,const unsigned long _shared_size){
+   kernel_mem_pool = _kernel_mem_pool;
+   process_mem_pool = _process_mem_pool;
+   shared_size = _shared_size;
+   current_page_table = new PageTable();
    Console::puts("Initialized Paging System\n");
 }
 
 PageTable::PageTable()
 {
-   assert(false);
+   unsigned long * page_dir = (unsigned long *) 0x9C000;
+   unsigned long * page_tab = (unsigned long * )  0x9D000;
+
+   unsigned long address = 0;
+   unsigned int i;
+
+   
+
+   //fill the first entry of the page directory
+   page_dir[0] = page_tab;
+   page_dir[0] = page_dir[0] | 3; //attribute set to: supervisor level, read/write, present
+   for(i = 0; i < 1024; i++){
+      page_tab[i] = address | 3; //attribute set to: supervisor level, read/write, present
+      address = address + 4096;
+   }
+   //map the first 4MB of memory
+   for(i = 1; i < 1024; i++){
+      page_dir[i] = 0 | 2; //attribute set to: supervisor level, read/write, not present
+   }
    Console::puts("Constructed Page Table object\n");
 }
 
 
 void PageTable::load()
 {
-   assert(false);
+   write_cr3(page_dir); //put the page directory address into CR3
    Console::puts("Loaded page table\n");
 }
 
 void PageTable::enable_paging()
 {
-   assert(false);
+   write_cr0(read_cr0 | 0x80000000); // set the paging bit in CR0 to 1
    Console::puts("Enabled paging\n");
 }
 
